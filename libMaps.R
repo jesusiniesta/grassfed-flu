35127db381ba1bc63b50c9d6b6931e3744737a4e
jesusiniesta <jesusiniesta@fastmail.com>
2015-03-11 00:51:42 +0100

    TOWN DETECTION: working in libMaps.R
    it still needs benchmarking and integration with the rest of the code

--- libMaps.R
+++ libMaps.R
@@ -3,11 +3,14 @@ library(rgeos)
 
 # https://stackoverflow.com/questions/21977720/r-finding-closest-neighboring-point-and-number-of-neighbors-within-a-given-rad/21981693#21981693
 
+htweets <- head(tweets, n=size);
 mydata <- select(htweets, lat, lon, user.name, msg)
 
 sp.mydata <- mydata
 coordinates(sp.mydata) <- ~lon+lat
 
+towns$lon <- towns$longitude
+towns$lat <- towns$latitude
 sp.towns <- towns
 coordinates(sp.towns) <- ~longitude+latitude
 
@@ -18,7 +21,7 @@ d <- gDistance(spgeom2=sp.mydata, spgeom1=sp.towns, byid=T)
 min.d <- apply(d, 1, function(x) order(x, decreasing=F)[1])
 
 # Construct new data frame with desired variables
-newdata <- cbind(mydata, sp.towns[min.d,])
+newdata <- cbind(mydata, towns[min.d,c("geonameid", "provincia", "asciiname", "population", "lon", "lat")])
 
 #colnames(newdata) <- c(colnames(mydata), "n.lat", "n.lon", "n.user.name", "n.msg", 'distance')
 
